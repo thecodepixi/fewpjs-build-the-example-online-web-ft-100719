15a0a11a9ade5a96fbcac8272fd2cb3fea7f8dec
Emily <emily.a.harber@gmail.com>
2020-01-30 12:05:04 -0500

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,51 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
 
+  function catchErrors(error){
+    let errorModal = document.getElementById("modal")
+    let messageModal = document.getElementById("modal-message")
+    messageModal.innerText = `${error.message}`
+    errorModal.classList.remove("hidden") 
+    setTimeout(function(){errorModal.classList.add("hidden")}, 5000)
+  }
+
+  // fake server call with error handling
+  mimicServerCall()
+  .then(json => console.log(json))
+  .catch(error => {
+    catchErrors(error)
+  })
+
+  function updateHeart(postId){
+    mimicServerCall()
+      .then(json => {
+        let post = document.getElementById(postId)
+        let like = post.getElementsByClassName("like-glyph")[0]
+        if (like.innerText == EMPTY_HEART) {
+          like.innerText = FULL_HEART;
+          like.classList.add("activated-heart")
+        } else {
+          like.innerText = EMPTY_HEART; 
+          like.classList.remove("activated-heart")
+        }
+      })
+      .catch( error => {
+        catchErrors(error);
+      })
+  }
+
+  let posts = document.getElementsByClassName("media-post")
+  for(i = 0; i < posts.length; i++) {
+    let like = posts[i].getElementsByClassName("like")[0]
+    let postId = posts[i].id
+    like.addEventListener("click", () => {
+      updateHeart(postId)
+    })
+  }
+
+})
 
 
 //------------------------------------------------------------------------------
